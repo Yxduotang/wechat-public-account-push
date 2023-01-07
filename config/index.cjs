11d0f71656f42ce9a8633b27a6789c9cc038f42f
Yxduotang <122138484+Yxduotang@users.noreply.github.com>
2023-01-07 13:37:13 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'owbFF6h3q_uH115__ezsqHrY7z8M',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'oECkrEpF7d2T6UJRJOnftkpisCylKbE1GLGg3FxXCXw',
+      useTemplateId: 'nhybGJRqrpLNkg1-XhxSwb4MZnfT6kfHQEjhxkRm2Cg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-02',
       festivals: [
