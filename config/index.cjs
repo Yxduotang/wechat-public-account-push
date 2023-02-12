9f5d53ca31c896e1453b2d9c9bf1e56985f2e0b6
Yxduotang <122138484+Yxduotang@users.noreply.github.com>
2023-02-12 17:35:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '婧宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'owbFF6uRatQ_tJZ1doZmaOVKq0zA',
+      id: 'owbFF6h3q_uH115__ezsqHrY7z8M',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'nhybGJRqrpLNkg1-XhxSwb4MZnfT6kfHQEjhxkRm2Cg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
